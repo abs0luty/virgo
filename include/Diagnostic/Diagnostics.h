38dfb4c34161348b9ee9cf13c13441f6af3d3302
Adi Salimgereev <adisalimgereev@gmail.com>
2023-12-25 22:37:24 +0600

    Add synchronization

--- include/Diagnostic/Diagnostics.h
+++ include/Diagnostic/Diagnostics.h
@@ -1,23 +1,57 @@
 #ifndef VIRGO_DIAGNOSTICS_H
 #define VIRGO_DIAGNOSTICS_H
 
+#include <unordered_map>
+#include <map>
 #include <vector>
+#include <mutex>
 #include "Diagnostic/Diagnostic.h"
 #include "Diagnostic/DiagnosticsEmitter.h"
 
 namespace virgo::diagnostic {
+    /*!
+     * A class that manages diagnostics synchronization in computations
+     * across threads like parallel parsing
+     */
     class Diagnostics final {
     public:
         Diagnostics() {}
 
+        /**
+         * @brief   Copy and then add a diagnostic to the diagnostics list
+         * @details Locks the mutex associated with the given file path
+         *          or when no file path is given locks the global diagnostics
+         *          mutex
+         * @param   diagnostic Diagnostic to add
+         */
         auto AddDiagnostic(const Diagnostic& diagnostic) -> void;
+
+        /**
+         * @brief   Add a diagnostic to the diagnostics list
+         * @details Locks the mutex associated with the given file path
+         *          or when no file path is given locks the global diagnostics
+         *          mutex
+         * @param   diagnostic Diagnostic to add
+         */
         auto AddDiagnostic(Diagnostic &&diagnostic) -> void;
-        auto Clear() -> void;
 
         ~Diagnostics() = default;
 
     private:
-        std::vector<Diagnostic> diagnostics;
+        /**
+         * Diagnostics not associated with any file.
+         */
+        std::vector<Diagnostic> globalDiagnostics;
+
+        /**
+         * Map of file paths to diagnostics data.
+         */
+        std::map<std::string, std::vector<Diagnostic>> fileDiagnostics;
+
+        /**
+         * Map of file paths to locks for related diagnostics data.
+         */
+        std::unordered_map<std::string, std::mutex> fileDiagnosticsLocks;
     };
 }
 
--- src/Diagnostic/Diagnostics.cpp
+++ src/Diagnostic/Diagnostics.cpp
@@ -1,15 +1,31 @@
 #include "Diagnostic/Diagnostics.h"
 
 namespace virgo::diagnostic {
-    auto Diagnostics::AddDiagnostic(const virgo::diagnostic::Diagnostic &diagnostic) -> void {
-        diagnostics.push_back(diagnostic);
+    auto Diagnostics::AddDiagnostic(const Diagnostic &diagnostic) -> void {
+        Diagnostic copy = diagnostic;
+        AddDiagnostic(std::move(copy));
     }
 
     auto Diagnostics::AddDiagnostic(Diagnostic &&diagnostic) -> void {
-        diagnostics.push_back(std::move(diagnostic));
-    }
+        if (!diagnostic.filepath) {
+            globalDiagnostics.push_back(std::move(diagnostic));
+            return;
+        }
 
-    auto Diagnostics::Clear() -> void {
-        diagnostics.clear();
+        if (fileDiagnosticsLocks.find(*diagnostic.filepath) == fileDiagnosticsLocks.end()) {
+            fileDiagnostics.insert(std::make_pair(*diagnostic.filepath, std::vector<Diagnostic>{}));
+            fileDiagnosticsLocks.emplace(
+                    std::piecewise_construct,
+                    std::make_tuple(*diagnostic.filepath),
+                    std::make_tuple()
+            );
+            return;
+        }
+
+        auto filepath = *diagnostic.filepath;
+
+        fileDiagnosticsLocks[filepath].lock();
+        fileDiagnostics[filepath].push_back(std::move(diagnostic));
+        fileDiagnosticsLocks[filepath].unlock();
     }
 }
